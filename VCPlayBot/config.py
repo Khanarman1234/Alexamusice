3ddf31fe55f87bfb82719ae51bfd72a1e8e601fc
Khanarman1234 <135990290+Khanarman1234@users.noreply.github.com>
2023-09-22 22:20:27 +0530

    Update config.py

--- VCPlayBot/config.py
+++ VCPlayBot/config.py
@@ -7,15 +7,15 @@ if os.path.exists("local.env"):
     load_dotenv("local.env")
 
 que = {}
-SESSION_NAME = getenv("SESSION_NAME", "session")
-BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_NAME = getenv("BOT_NAME")
+SESSION_NAME = getenv("SESSION_NAME", "1BVtsOKIBuwJIMK-prg3QrtJSB1J6zt5PVzrc7GiWyiYHY_eIf93aN0dVfyfb7ta8YHi_on4oowUy8j9eB7-Gctaz9qZdoBfQ5PLkRaurXRb24-e62it1lrctMUlZ9KNjXyKnV4rD8yMYVtBklkhLY-jjQf2VNySgrS2Mhnlyhb49z-OzJMBuI3YxC23VaWxLFBPm65RH9oXl21e2Ea12wgJ6woDYXsXT6N82hnA2EYbVj3CJJSF1WJmGaZAmmXHIhw_-fcZk-vsXEdhBCYJuEz1z4m_LXzE1oWG425HrExwPsiGQykM0y7OCMPdUmB3aAtzApevgWCvkrJ4rth2ZwPp7MBPgt-0=")
+BOT_TOKEN = getenv("BOT_TOKEN" ,"6431081969:AAHrnUc1me1SffMgcyvI9THYclgTiRgFE8A")
+BOT_NAME = getenv("BOT_NAME" ,"Nnnnn")
 UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "LaylaBots")
 BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/9b13ea3ce046a1a5c8098.png")
 admins = {}
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
-BOT_USERNAME = getenv("BOT_USERNAME")
+BOT_USERNAME = getenv("BOT_USERNAME" ,"Princess_music_robot")
 ASSISTANT_NAME = getenv("ASSISTANT_NAME", "Dead0XD")
 OWNER_NAME = getenv("OWNER_NAME", "HEROGAMERS1")
 SUPPORT_GROUP = getenv("SUPPORT_GROUP", "AwesomeSupport")
